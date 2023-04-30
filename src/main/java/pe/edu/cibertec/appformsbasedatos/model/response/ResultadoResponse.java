ced0eb073c5ba22d790caddf37daca98d0a33001
LUIS SALVATIERRA <luisangel.salvat@gmail.com>
2023-04-29 19:12:53 -0500

    Definición de objetos Request y Response, Repository y Service.

---
+++ src/main/java/pe/edu/cibertec/appformsbasedatos/model/response/ResultadoResponse.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.appformsbasedatos.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResultadoResponse {
+    private Boolean respuesta;
+    private String mensaje;
+}
